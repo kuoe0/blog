54b1e395068412eaa8b9041751ed1c49128888dc
KuoE0 <kuoe0.tw@gmail.com>
2016-04-01 03:05:46 +0800

    Support the relative path for the feature photo.

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -1,11 +1,28 @@
+'use strict'
+
 jQuery(document).ready(function($){
   function setFeaturePhoto() {
     if (typeof featurePhoto === 'undefined')
       return;
     console.log('Feature photo: ' + JSON.stringify(featurePhoto));
-    $('#page-wrapper').css('background-image', 'url(' + featurePhoto.photo_url + ')');
+    $('#page-wrapper').css('background-image', 'url(' + featurePhoto.photoUrl + ')');
     $('header.main-header').css('background', 'rgba(0, 0, 0, 0.5)');
   }
 
+  // Only used for homepage.
+  function updateAllThumbPhoto() {
+    $('.thumb-photo').each((idx, elem) => {
+      let location = window.location;
+      var oldUrl = $(elem).attr('src');
+      var newUrl = Helper.getFeaturePhotoUrl(location.protocol + '//' + location.host,
+                                             location.pathname,
+                                             oldUrl);
+      if (oldUrl != newUrl) {
+        $(elem).attr('src', newUrl);
+      }
+    });
+  }
+
   setFeaturePhoto();
+  updateAllThumbPhoto();
 });
