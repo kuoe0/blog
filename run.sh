dcfdceede08afdc13ad0b669d243e889066adb65
KuoE0 <kuoe0.tw@gmail.com>
2016-05-10 01:14:00 +0800

    fixed baseurl in _config-dev.yml not working

--- run.sh
+++ run.sh
@@ -16,8 +16,9 @@ if [ "$?" -eq "0" ]; then
   curl -s "https://pages.github.com/versions.json" > gh-pages-versions.json
 fi
 
+# XXX baseurl in _config-dev.yml not working, use --baseurl instead of it.
 if [[ "$1" = "debug" ]]; then
-  bundle exec jekyll serve --watch --config "_config.yml,_config-dev.yml" -V
+  bundle exec jekyll serve --watch --config "_config.yml,_config-dev.yml" --baseurl="" -V
 else
-  bundle exec jekyll serve --watch --config "_config.yml,_config-dev.yml"
+  bundle exec jekyll serve --watch --config "_config.yml,_config-dev.yml" --baseurl=""
 fi
