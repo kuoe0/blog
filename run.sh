be03f5eeecfdb8010a5d9f03b6c26650ba4ad085
KuoE0 <kuoe0.tw@gmail.com>
2016-05-10 01:11:36 +0800

    add debug mode

--- run.sh
+++ run.sh
@@ -15,6 +15,9 @@ curl -s "https://pages.github.com/versions.json" > /dev/null
 if [ "$?" -eq "0" ]; then
   curl -s "https://pages.github.com/versions.json" > gh-pages-versions.json
 fi
-bundle exec jekyll serve --watch --config _config.yml,_config-dev.yml
 
-
+if [[ "$1" = "debug" ]]; then
+  bundle exec jekyll serve --watch --config "_config.yml,_config-dev.yml" -V
+else
+  bundle exec jekyll serve --watch --config "_config.yml,_config-dev.yml"
+fi
