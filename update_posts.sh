61bc72cf711cc34eb304d37f4c0148e915360db9
KuoE0 <kuoe0.tw@gmail.com>
2016-05-16 13:24:31 +0800

    add script to update posts

---
+++ update_posts.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# update_posts.sh
+# KuoE0 <kuoe0.tw@gmail.com>
+# Copyright (C) 2016
+#
+# Distributed under terms of the MIT license.
+#
+
+git submodule init
+git submodule update
+git submodule foreach git pull origin master -f
