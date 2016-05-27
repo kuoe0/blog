5544191f509f0b80b0547a0c7b170eb3be2af00a
KuoE0 <kuoe0.tw@gmail.com>
2016-05-27 17:51:30 +0800

    remove useless code

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -45,20 +45,5 @@ jQuery(document).ready(function($){
     $('header.main-header').css('background', 'rgba(0, 0, 0, 0.5)');
   }
 
-  // Only used for homepage.
-  function updateAllThumbPhoto() {
-    $('.thumb-photo').each((idx, elem) => {
-      var location = window.location;
-      var oldUrl = $(elem).attr('src');
-      var newUrl = Helper.getFeaturePhotoUrl(location.protocol + '//' + location.host,
-                                             location.pathname,
-                                             oldUrl);
-      if (oldUrl != newUrl) {
-        $(elem).attr('src', newUrl);
-      }
-    });
-  }
-
   setFeaturePhoto();
-  updateAllThumbPhoto();
 });
